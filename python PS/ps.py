9554b28b01acbfb9b8829d5d39becd3ff0042e15
PrathibhaM1011 <prathibhamamindla2910@gmail.com>
2025-02-20 16:17:09 +0530

    Update ps.py

--- python PS/ps.py
+++ python PS/ps.py
@@ -90,15 +90,6 @@ elif operation == '/':
 else:
     print("Invalid operation")
 
-# 8. Display the name of a month based on a number -- USED CHATGPT for alternative method
-month_number = int(input("Enter a month number (1-12): "))
-months = {
-    1: "January", 2: "February", 3: "March", 4: "April",
-    5: "May", 6: "June", 7: "July", 8: "August",
-    9: "September", 10: "October", 11: "November", 12: "December"
-}
-print(months.get(month_number, "Invalid input"))
-
 
 # medium Questions
 # 5. to check the sides form a triangle or not
