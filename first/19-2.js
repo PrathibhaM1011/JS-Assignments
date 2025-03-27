5cb676cd334a7a5019c57a0d43d512367514235b
PrathibhaM1011 <prathibhamamindla2910@gmail.com>
2025-03-27 21:53:22 +0530

    Add files via upload

--- first/19-2.js
+++ first/19-2.js
@@ -1,38 +1,105 @@
-// first question//
 
 
-function processNumbers(a, b, callback) {
-    return callback(a, b);
+
+// console.log("hai")
+
+// // first question//
+
+
+// function processNumbers(a, b, callback) {
+//     return callback(a, b);
+// }
+
+// var result = processNumbers(3, 4, function(x, y) {
+//     return x + y;
+// });
+
+// console.log("Sum:", result);
+
+// // second question//
+
+// function greet(callback) {
+//     return callback("Alice");
+// }
+
+// var result = greet(function(name) {
+//     return "Hello, " + name + "!";
+// });
+
+// console.log(result);
+
+// // third question//
+
+// function calculate(a, b, callback) {
+//     return callback(a, b);
+// }
+
+// var result = calculate(10, 5, function(x, y) {
+//     return x - y;
+// });
+
+// console.log("Difference:", result);
+
+function processOrder(orderId, callback){
+    if (orderId == ''){
+        callback(false, "Order Process failed")
+    }
+    else{
+        callback(true,"Order Processed Successfully")
+    }
 }
 
-var result = processNumbers(3, 4, function(x, y) {
-    return x + y;
-});
-
-console.log("Sum:", result);
-
-// second question//
-
-function greet(callback) {
-    return callback("Alice");
+function shipOrder(orderId, callback){
+    if (orderId == "FAIL"){
+        callback(false, "Shipping failed")
+    }
+    else{
+        callback(true,"Order shipped with Tracking Id:XYZ123")
+    }
+}
+function dispatchOrder(orderId, callback){
+    if (orderId == "DELAY"){
+        callback(false, "Dispatch Failed")
+    }
+    else{
+        callback(true, "Order dispatched with vehicle-1")
+    }
 }
 
-var result = greet(function(name) {
-    return "Hello, " + name + "!";
-});
-
-console.log(result);
-
-// third question//
-
-function calculate(a, b, callback) {
-    return callback(a, b);
+function outForDelivery(orderId, callback){
+    if (orderId == "CANCEL"){
+        callback(false, "Delivery Failed")
+    }
+    else{
+        callback(true, "Order is out for Delivery")
+    }
 }
 
-var result = calculate(10, 5, function(x, y) {
-    return x - y;
-});
-
-console.log("Difference:", result);
-
-
+processOrder("123" , (status, message) => {
+    if(status == true){
+        console.log(message);
+        shipOrder("PASS", (status, message)=>{
+            if(status == true){
+                console.log(message)
+            dispatchOrder("DELAY", (status, message)=>{
+                if(status == true){
+                    console.log(message)
+                outForDelivery("Success", (status, message)=>{
+                    if(status == true){
+                        console.log(message)
+                    }else{
+                        console.log(message)
+                    }
+                })
+                }else{
+                    console.log(message)
+                }
+            })
+            }else{
+                console.log(message)
+            }
+        })
+    }else{
+        console.log(message);
+    }
+} )
